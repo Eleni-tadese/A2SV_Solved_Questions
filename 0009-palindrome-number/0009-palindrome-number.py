958f25bbb065793d7739c6e61b3573118e6f9d49
Eleni Tadese <elenitade1221@gmail.com>
2026-02-05 10:25:14 -0800

    Time: 16 ms (30.32%), Space: 12.4 MB (55.41%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,22 @@
 class Solution(object):
     def isPalindrome(self, x):
-        num=str(x)
-        snum=num[::-1]
-        return snum==num
-   
-        
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        divider =1
+        while x >= divider * 10:
+            divider *= 10
+        while x>0:
+            l=x//divider
+            r= x % 10
+            
+            if r!=l:
+                return False
+            x = (x % divider) // 10
+            divider //= 100
+        return True
+
+     
