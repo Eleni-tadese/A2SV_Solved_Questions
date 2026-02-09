d2cdedd56624309ca685664cdc5baeb5edd6c8fd
Eleni Tadese <elenitade1221@gmail.com>
2026-02-09 09:04:06 -0800

    Time: 12 ms (34.3%), Space: 20.8 MB (5.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,37 +1,19 @@
 class Solution:
-    def twoSum(self, arr: List[int], target: int) -> List[int]:
-        l,r=0,len(arr)-1
-        new=[]
-        for index,value in enumerate(arr):
-            new.append([value,index])
-        new.sort()
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        list_nums=[]
+        for v,i in enumerate(nums):
+            list_nums.append([i,v])
+        print(list_nums)
+
+        list_nums.sort()
+        l=0
+        r=len(nums)-1
+       
         while l<r:
-            if new[r][0]+new[l][0]>target:
+            if list_nums[r][0]+ list_nums[l][0]>target:
                 r-=1
-            elif new[r][0]+new[l][0]<target:
+            elif list_nums[r][0]+ list_nums[l][0]<target:
                 l+=1
             else:
-                return [new[l][1],new[r][1]]
-        # dict = {}
-        # for i, num in enumerate(nums):
-        #     diffrence = target - num
-        #     if diffrence in dict:
-        #         return [dict[diffrence], i]
-        #     dict[num] = i
-# class Solution:
-#     def twoSum(self, arr: List[int], target: int) -> List[int]:
-#         neww = [[val, idx] for idx, val in enumerate(arr)]
-#         neww.sort()   # sort by value
-        
-#         l, r = 0, len(arr) - 1
-        
-#         while l < r:
-#             s = neww[l][0] + neww[r][0]
-            
-#             if s == target:
-#                 return [neww[l][1], neww[r][1]]
-            
-#             if s < target:
-#                 l += 1
-#             else:
-#                 r -= 1
+                return [list_nums[l][1],list_nums[r][1]]
+      
