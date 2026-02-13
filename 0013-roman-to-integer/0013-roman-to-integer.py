74606287e0660023d950a27a80e67be8f81119e7
Eleni Tadese <elenitade1221@gmail.com>
2026-02-13 03:48:23 -0800

    Time: 4 ms (64.53%), Space: 19.5 MB (14.24%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,18 +2,23 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         # Step 1: Map symbols to values
         roman_map = {
-            'I': 1, 'V': 5, 'X': 10,
-            'L': 50, 'C': 100, 'D': 500, 'M': 1000
+            'I': 1,
+             'V': 5, 
+             'X': 10,
+            'L': 50, 
+            'C': 100, 
+            'D': 500, 
+            'M': 1000
         }
         
-        total = 0
-        n = len(s)
-        
+        number = 0
+    
+        n=len(s)
         for i in range(n):
-            # Step 2: If current < next, subtract, else add
-            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
-                total -= roman_map[s[i]]
-            else:
-                total += roman_map[s[i]]
         
-        return total
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                number -= roman_map[s[i]]
+            else:
+                number += roman_map[s[i]]
+        
+        return number
