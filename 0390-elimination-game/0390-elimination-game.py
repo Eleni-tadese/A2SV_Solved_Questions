c0cc46f33db3171baa5f6eeb45d7870af95ddfb1
Eleni Tadese <elenitade1221@gmail.com>
2026-03-19 08:26:56 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.py
+++ 0390-elimination-game/0390-elimination-game.py
@@ -1,6 +1,22 @@
 class Solution:
     def lastRemaining(self, n: int) -> int:
-        if n ==1:
-            return 1
-        else:
-            return 2 * (1 + n // 2 - self.lastRemaining(n // 2))
+    
+
+        current  =  1
+        step = 1
+
+        remaining = n
+        left_to_right = True
+
+        while remaining > 1:
+
+            if left_to_right or remaining % 2 == 1:
+
+                current+=step
+
+            step*=2
+            remaining//=2
+
+            left_to_right = not left_to_right
+        
+        return current
