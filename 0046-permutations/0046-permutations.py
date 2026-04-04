ff8e52af7407a1f2e2a0b0505ed624b2f44ae7cd
Eleni Tadese <eleni.tadese@astu.edu.et>
2026-04-04 11:48:42 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(path, used):
+            
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+
+
+            for num in nums:
+                if num not in used:
+                    used.add(num)
+                    path.append(num)
+                    backtrack(path, used)
+                    path.pop()
+                    used.remove(num)
+
+        backtrack([], set())
+        return res
